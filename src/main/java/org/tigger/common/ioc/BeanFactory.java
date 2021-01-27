576e4c177afdee056d9dc072718f945a1d9e61dc
shaoyangdd <shaoyangdd@163.com>
2021-01-28 00:38:12 +0800

    IOC容器实现

--- src/main/java/org/tigger/common/ioc/BeanFactory.java
+++ src/main/java/org/tigger/common/ioc/BeanFactory.java
@@ -1,5 +1,9 @@
 package org.tigger.common.ioc;
 
+import org.tigger.common.util.PackageUtil;
+
+import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -16,13 +20,55 @@ public class BeanFactory {
 
 
     public void autowireBean() {
-
         //扫描包下所有的bean List<Class> 不包含接口，枚举
-
-        //（反射）按依赖的本包下的类的个数排序，依赖的少的在前，先组装依赖的少的，（TODO 先不解决循环依赖）
-
-        //（反射）依次组装bean,并注册到Map
-
+        List<String> className = PackageUtil.getClassName("org.tigger");
+        for (String s : className) {
+            beanMap.put(loadClass(s), null);
+        }
+        beanMap.forEach((k, v) -> {
+            instant(beanMap, k, 0);
+        });
     }
 
+    private Class loadClass(String className) {
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 实例化
+     *
+     * @param beanMap
+     * @param clazz
+     * @param count   递归次数，超过N就是循环依赖，做中断处理
+     * @return
+     */
+    private void instant(Map<Class, Object> beanMap, Class clazz, int count) {
+        if (count > 100) {
+            throw new RuntimeException("出现了循环依赖，请检查!");
+        }
+        Object obj = getInstanceByClass(clazz);
+        beanMap.put(clazz, obj);
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            if (beanMap.containsKey(field.getType())) {
+                //是包下的类
+                Object objField = beanMap.get(field.getType());
+                if (objField == null) {
+                    instant(beanMap, field.getType(), count++);
+                }
+            }
+        }
+    }
+
+    private Object getInstanceByClass(Class clazz) {
+        try {
+            return clazz.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
