ff43de8633964adc16c4bcd5d6977300704f8b0f
shaoyangdd <shaoyangdd@163.com>
2021-01-28 01:03:45 +0800

    IOC容器实现

--- src/main/java/org/tigger/common/ioc/BeanFactory.java
+++ src/main/java/org/tigger/common/ioc/BeanFactory.java
@@ -16,21 +16,35 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class BeanFactory {
 
-    private Map<Class, Object> beanMap = new ConcurrentHashMap<>();
+    /**
+     * bean容器
+     */
+    private static final Map<Class<?>, Object> beanMap = new ConcurrentHashMap<>();
 
-
+    /**
+     * 自动实例化所有的本包下的bean
+     */
     public void autowireBean() {
-        //扫描包下所有的bean List<Class> 不包含接口，枚举
+        //1. 扫描包下所有的bean List<Class> 不包含接口，枚举
         List<String> className = PackageUtil.getClassName("org.tigger");
+        //2. 实例化所有bean
         for (String s : className) {
-            beanMap.put(loadClass(s), null);
+            Class<?> clazz = loadClass(s);
+            beanMap.put(clazz, getInstanceByClass(clazz));
         }
+        //3. 注入依赖
         beanMap.forEach((k, v) -> {
-            instant(beanMap, k, 0);
+            inject(beanMap, k, v, 0);
         });
     }
 
-    private Class loadClass(String className) {
+    /**
+     * 加载class
+     *
+     * @param className 类名 xx.xx.Abc
+     * @return class
+     */
+    private Class<?> loadClass(String className) {
         try {
             return Class.forName(className);
         } catch (ClassNotFoundException e) {
@@ -39,32 +53,40 @@ public class BeanFactory {
     }
 
     /**
-     * 实例化
+     * 依赖注入
      *
-     * @param beanMap
-     * @param clazz
+     * @param beanMap bean容器
+     * @param clazz   类对象
      * @param count   递归次数，超过N就是循环依赖，做中断处理
-     * @return
      */
-    private void instant(Map<Class, Object> beanMap, Class clazz, int count) {
+    private void inject(Map<Class<?>, Object> beanMap, Class<?> clazz, Object obj, int count) {
+        //简单粗暴的判断循环依赖
         if (count > 100) {
             throw new RuntimeException("出现了循环依赖，请检查!");
         }
-        Object obj = getInstanceByClass(clazz);
-        beanMap.put(clazz, obj);
         Field[] fields = clazz.getDeclaredFields();
         for (Field field : fields) {
-            if (beanMap.containsKey(field.getType())) {
-                //是包下的类
-                Object objField = beanMap.get(field.getType());
-                if (objField == null) {
-                    instant(beanMap, field.getType(), count++);
+            Object fieldValue = beanMap.get(field.getType());
+            if (fieldValue != null) {//是包下的类
+                field.setAccessible(true);
+                try {
+                    field.set(obj, fieldValue);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
                 }
+                //递归注入
+                inject(beanMap, field.getType(), fieldValue, ++count);
             }
         }
     }
 
-    private Object getInstanceByClass(Class clazz) {
+    /**
+     * 根据类对象获取实例对象
+     *
+     * @param clazz 类对象
+     * @return 实例对象
+     */
+    private Object getInstanceByClass(Class<?> clazz) {
         try {
             return clazz.newInstance();
         } catch (Exception e) {
